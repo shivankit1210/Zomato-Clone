0c2a117eb5338624a802644f82ed953d97078df2
Shivankit Agarwal <shivankit1210@gmail.com>
2024-09-19 16:03:28 +0530

    ADDED several section like collection , popular places, installation steps

---
+++ script.js
@@ -0,0 +1,18 @@
+function showInput() {
+    const emailField = document.getElementById('emailField');
+    const phoneField = document.getElementById('phoneField');
+    const emailOption = document.getElementById('emailOption').checked;
+    const phoneOption = document.getElementById('phoneOption').checked;
+
+    // Show email field if 'Email' radio button is selected, hide phone field
+    if (emailOption) {
+        emailField.style.display = 'block';
+        phoneField.style.display = 'none';
+    }
+
+    // Show phone field if 'Phone' radio button is selected, hide email field
+    if (phoneOption) {
+        phoneField.style.display = 'block';
+        emailField.style.display = 'none';
+    }
+}
